841b573ad3ad80b3103a79351016fd074b571deb
Dave-Njeru <davenjeru9@gmail.com>
2024-12-22 22:15:33 +0300

    add 404 page

---
+++ public/views/404.php
@@ -0,0 +1,11 @@
+<?php
+$header = 'Page not found';
+
+require 'partials/head.php';
+?>
+
+<div class="md:container mx-auto mt-3 px-4">
+    <h2>The page you requested was not found.
+        <a href="<?= BASE_URL ?>" class="underline decoration-pink-500 text-blue-400">Go back home</a>
+    </h2>
+</div>
