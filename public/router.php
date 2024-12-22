23447a4f4552cc47a69ce63ef205b3d35db9faa3
Dave-Njeru <davenjeru9@gmail.com>
2024-12-22 22:18:18 +0300

    modified router

--- public/router.php
+++ public/router.php
@@ -5,14 +5,19 @@ $url = parse_url($_SERVER['REQUEST_URI'])['path'];
 $routes = [
     '/projects/carestream/public/' => 'controllers/index.php',
     '/projects/carestream/public/login' => 'controllers/login.php',
+    '/projects/carestream/public/register' => 'controllers/register.php',
 ];
 
 function routeToController($url, $routes) {
     if (array_key_exists($url, $routes)) {
         require $routes[$url];
     } else {
-        echo 'Route not found';
+        abort();
     }
+}
+
+function abort() {
+    require 'views/404.php';
 }
 
 routeToController($url, $routes);
