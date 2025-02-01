b3a232a71e1aa2b2cd6c4fd9b527ef39cd591355
Dave-Njeru <davenjeru9@gmail.com>
2025-02-01 14:23:57 +0300

    implement login

--- public/views/login.view.php
+++ public/views/login.view.php
@@ -2,23 +2,52 @@
 $header = 'Login';
 
 require 'partials/head.php';
+require 'connection.php';
 
-if(isset($_GET['redirect'])) {
+if (isset($_GET['redirect'])) {
     alert("Email verification successful. You can proceed to login.");
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+        $username = test_input($_POST['username']);
+        $password = test_input($_POST['password']);
+
+        $sql = "select * from patients where email = ?";
+
+        if($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+
+            $result  = $stmt->get_result();
+
+            if($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                //check password against stored hash
+                if(password_verify($password, $row['password']) && $username === $row['email']) {
+                    alert("Valid credentials");
+                } else {
+                    alert("Invalid credentials");
+                }
+            }
+        }
+    } catch (Exception $e) {
+        alert("Error: " . $e->getMessage());
+    }
 }
 ?>
 
 <div class="md:container mx-auto px-4 flex items-center justify-center h-full"> <!--content wrapper-->
     <div class="bg-white w-1/2 p-16">
         <h2 class="text-center text-3xl fav-color font-bold">Welcome Back!</h2>
-        <form action="#">
+        <form action="/projects/carestream/public/login" method="POST">
             <div>
                 <!--Email-->
-                <input type="text" name="" id="" class="form-input" placeholder="Email">
+                <input type="email" name="username" class="form-input" placeholder="Email">
             </div>
             <div>
                 <!--Password-->
-                <input type="password" name="" id="" class="form-input" placeholder="Password">
+                <input type="password" name="password" class="form-input" placeholder="Password">
             </div>
             <div class="text-center">
                 <button type="submit" class="border-solid border-2 my-3 p-3 rounded-lg fav-bg">
@@ -26,7 +55,7 @@ if(isset($_GET['redirect'])) {
                 </button>
             </div>
         </form>
-        
+
         <div class="text-center text-sm">
             <a href="#" class="fav-color">Forgot password?
         </div>
