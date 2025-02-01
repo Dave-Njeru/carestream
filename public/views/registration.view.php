36ae8aa9bbec98f9283bf1da4acd43c65119ee75
Dave-Njeru <davenjeru9@gmail.com>
2025-02-01 14:22:29 +0300

    code refactor

--- public/views/registration.view.php
+++ public/views/registration.view.php
@@ -63,38 +63,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <div class="md:container mx-auto px-4 flex items-center justify-center h-full"> <!--content wrapper-->
     <div class="bg-white w-1/2 p-4">
         <h2 class="text-center text-3xl fav-color font-bold">Registration Form</h2>
-        <form action="" method="post">
-            <form action="/projects/carestream/public/register" method="post">
-                <div>
-                    <!--First Name-->
-                    <input type="text" name="first_name" class="form-input" placeholder="First Name" pattern="[A-Za-z]{1,32}" title="Only alphabet letters are allowed" required>
-                </div>
-                <div>
-                    <!--Last Name-->
-                    <input type="text" name="last_name" class="form-input" placeholder="Last Name" pattern="[A-Za-z]{1,32}" title="Only alphabet letters are allowed" required>
-                </div>
-                <div>
-                    <!--Email-->
-                    <input type="text" name="email" id="email" class="form-input" placeholder="Email" required>
-                </div>
-                <div>
-                    <!--Contact-->
-                    <input type="tel" name="contact" class="form-input" placeholder="Contact eg. 254111222333" id="contact" required>
-                </div>
-                <div>
-                    <!--Password-->
-                    <input type="password" name="password" class="form-input" id="password" placeholder="Password" required>
-                </div>
-                <div class="text-center">
-                    <button type="submit" class="border-solid border-2 my-3 p-3 rounded-lg fav-bg">
-                        <span class="uppercase text-white">create account</span>
-                    </button>
-                </div>
-            </form>
-
-            <div class="text-center text-sm">
-                Already have an account? <a href="#" class="fav-color">Sign in</a>
+        <form action="/projects/carestream/public/register" method="POST">
+            <div>
+                <!--First Name-->
+                <input type="text" name="first_name" class="form-input" placeholder="First Name" pattern="[A-Za-z]{1,32}" title="Only alphabet letters are allowed" required>
             </div>
+            <div>
+                <!--Last Name-->
+                <input type="text" name="last_name" class="form-input" placeholder="Last Name" pattern="[A-Za-z]{1,32}" title="Only alphabet letters are allowed" required>
+            </div>
+            <div>
+                <!--Email-->
+                <input type="text" name="email" id="email" class="form-input" placeholder="Email" required>
+            </div>
+            <div>
+                <!--Contact-->
+                <input type="tel" name="contact" class="form-input" placeholder="Contact eg. 254111222333" id="contact" required>
+            </div>
+            <div>
+                <!--Password-->
+                <input type="password" name="password" class="form-input" id="password" placeholder="Password" required>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="border-solid border-2 my-3 p-3 rounded-lg fav-bg">
+                    <span class="uppercase text-white">create account</span>
+                </button>
+            </div>
+        </form>
+
+        <div class="text-center text-sm">
+            Already have an account? <a href="#" class="fav-color">Sign in</a>
+        </div>
     </div>
 </div>
 </div>
