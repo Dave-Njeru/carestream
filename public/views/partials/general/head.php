54340be3270b08663c2a4638c98009718ef85392
Dave-Njeru <davenjeru9@gmail.com>
2025-06-13 11:43:30 +0300

    feat: new version for datatable plugin

--- public/views/partials/general/head.php
+++ public/views/partials/general/head.php
@@ -6,12 +6,16 @@
     <title><?= $header ?> </title>
     <link rel="stylesheet" href="<?= BASE_URL; ?>/output.css">
     <!--fontawesome-->
-    <script src="https://kit.fontawesome.com/d0bdada029.js" crossorigin="anonymous"></script> 
+    <script src="https://kit.fontawesome.com/d0bdada029.js" crossorigin="anonymous"></script>
     <!--jquery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <!--datatables plugins -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" />
-    <script src="https://cdn.datatables.net/2.2.2/js/dataTables.js" defer></script>
+    <!-- <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.2.2/js/dataTables.js" defer></script> -->
+
+    <!-- New version for datatables plugin V2.3.2 -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
     <!--sweet alert-->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
